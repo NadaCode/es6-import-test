c4013eb1cba3b256aae4ea14b0899268d8ce845a
Pasi Mankinen <pasi.mankinen@manageapp.com>
2018-11-01 04:30:09 +0200

    Test script defer, html need to be parsed before scripts run.

--- es6_test.js
+++ es6_test.js
@@ -2,7 +2,7 @@
  * es6_test.js: json formatting and es6 rollup test
  *-------------------------------------------------------------------------------------------- */
 
-import { test } from './es6_test_lib.js'
+// import { test } from './es6_test_lib.js'
 import stringify from './lib/json-stringify-pretty-compact.js'
 // const stringify = require('./node_modules/json-stringify-pretty-compact/index.js') // does not work in browsers
 
@@ -16,5 +16,27 @@ const jsonObj = {
 	},
 }
 
-debugger
+const testStringifyPrettyCompact = (json, stringify) => {
+	let options = { maxLength: 80, indent: 2, }
+	return stringify(json, options)
+}
+
+export function test (json, stringify) {
+	const isNode = typeof window === 'undefined'
+	let jsonTxt = JSON.stringify(json)
+	console.log('jsonTxt:', jsonTxt)
+	let formattedJson = testStringifyPrettyCompact(json, stringify)
+	console.log('jsonTxt formatted compact:', formattedJson)
+	debugger
+	if (isNode === false) {
+		const elem1 = document.getElementById('json')
+		if (!elem1) console.error("json test: document.getElementById('json') failed")
+		else {
+			elem1.innerHTML = jsonTxt
+			const elem2 = document.getElementById('formattedJson')
+			elem2.innerHTML = formattedJson
+		}
+	}
+}
+
 test(jsonObj, stringify)
